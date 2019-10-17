c9b6690ad5ce4313d576996258988a1a0677b9ff
laurenchilders <chilla17@wfu.edu>
2019-10-17 15:30:16 -0400

    Complete

---
+++ cmake-build-debug/CMakeFiles/RuleOfThree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RuleOfThree.dir/main.cpp.o"
+  "CMakeFiles/RuleOfThree.dir/IntList.cpp.o"
+  "RuleOfThree.pdb"
+  "RuleOfThree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RuleOfThree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
